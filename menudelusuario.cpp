cbdba363b23f7c63c29656fc6433df435f33ad7a
Evan Adrián Gutiérrez Solís <00008726@uca.edu.sv>
2026-06-15 19:05:54 -0600

    :memo: corregido error de compilación

--- menudelusuario.cpp
+++ menudelusuario.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int opcion, categoria, respuesta;
+    int opcion = 0, categoria = 0, respuesta;
 
     do
     {
@@ -14,13 +15,15 @@ int main()
         cout << "5. Modificar pedido" << endl;
         cout << "6. Salir" << endl;
         cin >> opcion;
+
         if (opcion < 1 || opcion > 6)
         {
             cout << "Opcion invalida" << endl;
         }
         else
         {
-            switch (opcion) {
+            switch (opcion)
+            {
             case 1:
             {
                 cout << "\n======CATEGORIAS======" << endl;
@@ -30,22 +33,34 @@ int main()
                 cout << "4. Snacks" << endl;
                 cout << "5. Bebidas" << endl;
                 cin >> categoria;
-                if(categoria<1 || categoria>5){
-                    cout<<"Categoria invalida"<<endl;
-                } else { 
-                    switch(categoria) {
-                        case 1:
-                        cout<<"\n======DESAYUNOS======"<<endl;
-                        cout<<"1. Desayuno tipico----$3.50"<<endl;
-                        cout<<"2. Pan con huevo----$1.50"<<endl;
-                        cout<<"3. Pan con frijoles y queso----$2.00"<<endl;
-                        cout<<"4. Pancakes----$2.50"<<endl;
-                        cout<<"Sandwich de jamon----$2.50"<<endl;
+                if (categoria < 1 || categoria > 5)
+                {
+                    cout << "Categoria invalida" << endl;
+                }
+                else
+                {
+                    switch (categoria)
+                    {
+                    case 1:
+                        cout << "\n======DESAYUNOS======" << endl;
+                        cout << "1. Desayuno tipico----$3.50" << endl;
+                        cout << "2. Pan con huevo----$1.50" << endl;
+                        cout << "3. Pan con frijoles y queso----$2.00" << endl;
+                        cout << "4. Pancakes----$2.50" << endl;
+                        cout << "5. Sandwich de jamon----$2.50" << endl;
                         break;
-                        
+                    default:
+                        cout << "Opcion de desayuno no implementada." << endl;
+                        break;
                     }
                 }
-
+                break;
+            }
+            case 6:
+                cout << "Saliendo..." << endl;
+                break;
+            default:
+                cout << "Opcion no implementada aun." << endl;
                 break;
             }
         }
